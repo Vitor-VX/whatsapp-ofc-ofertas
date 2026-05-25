f7be8a6329ccbf194f7270d90f0d47f964f8f386
Vitor-Vx <joaovictorrocha102019@gmail.com>
2026-05-24 23:05:54 -0400

    Adicionado nova logica de validacao + logs

--- src/utils/isValidYoutube.ts
+++ src/utils/isValidYoutube.ts
@@ -1,9 +1,15 @@
 export function isYouTubeUrl(url: string): boolean {
+    if (!url.startsWith("http")) return false;
+
     try {
         const parsed = new URL(url);
+        console.log(parsed);
+        
+        const hostname = parsed.hostname.replace("www.", "");
+
         return (
-            parsed.hostname.includes("youtube.com") ||
-            parsed.hostname.includes("youtu.be")
+            hostname === "youtube.com" ||
+            hostname === "youtu.be"
         );
     } catch {
         return false;
